b3a3b5eedd08d94bda4bc173afd380804229e112
ross1705 <rossjackson1705@gmail.com>
2023-10-23 22:53:06 +0100

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FileUpload from './FileUpload';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Supabase File Upload</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
